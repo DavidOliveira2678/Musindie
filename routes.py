9ac5633cf26d87f7344eacc7d2172b8079639037
klarisse <klarissemaria11@gmail.com>
2025-07-13 19:08:36 -0300

    atualização de códigos + front

--- routes.py
+++ routes.py
@@ -1,25 +1,25 @@
 from main import app    
 from flask import Flask , render_template
 
-
-
 @app.route("/")
 def homepage():
-    return "Página incial do site!"
+    total_categories = len(games_categories)
+    total_games = sum(len(jogos)for jogos in games_categories.values())
+    return render_template("home.html", total_categories= total_categories, total_games = total_games)
 
-@app.route("/category/<nome>")
-def categoria(nome):
-   
-   games_categories = {
-    "RPG":["seaoftears","undertale","masseffect"],
-    "Indie":["celeste","stardewvalley","indivisibleswitch","geshinimpact"],
-    "Acao&Aventura":["blasphemous","astroneer","eternalstrands"]
+games_categories = {
+    "RPG":["sea_of_tears","undertale","masse_ffect"],
+    "Indie":["celeste","stardew_valley","indivisible_switch","geshin_impact"],
+    "acao-aventura":["blasphemous","astroneer","eternal_strands"],
+    "Corrida":["forza_horizon","crash_team_racing","f1","the_crew_motorfest"],
+    "Luta":["mortal_kombat","dragon_ball","guilty_gear","undisputed","brawlhalla"]
    } 
+@app.route("/category/<nome>")
 
+def categoria(nome):
    lista_jogos = games_categories.get(nome , [])
- 
    return render_template("category.html", categoria=nome , jogos=lista_jogos)
-   
+  
 @app.route("/blog")
 def blog():
     return render_template("blog.html")
