39bc0d64c8800b54f61e8430f26541cd42234e5b
klarisse <klarissemaria11@gmail.com>
2025-07-15 20:24:37 -0300

    correções
    ajustando nome das funções para o inglês e variáveis.

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ from flask import Flask , render_template
 @app.route("/")
 def homepage():
     total_categories = len(games_categories)
-    total_games = sum(len(jogos)for jogos in games_categories.values())
+    total_games = sum(len(games)for games in games_categories.values())
     return render_template("home.html", total_categories= total_categories, total_games = total_games)
 
 ## Jogos e categorias
@@ -13,16 +13,20 @@ games_categories = {
     "RPG":["sea of tears","undertale","mass effect"],
     "Indie":["celeste","stardew valley","indivisible switch","sword of the sea"],
     "acao-aventura":["blasphemous","astroneer","eternal strands"],
-    "Corrida":["forza horizon","crash team racing","f1","the crew motorfest"],
+    "corrida":["forza horizon","crash team racing","f1","the crew motorfest"],
     "Luta":["mortal kombat","dragon ball","guilty gear","undisputed","brawlhalla"]
    }
 
 ## Rota Categoria de jogo
 @app.route("/category/<nome>")
 
-def categoria(nome):
-   lista_jogos = games_categories.get(nome , [])
-   return render_template("category.html", categoria=nome , jogos=lista_jogos)
+def game_category(nome):
+   games_list = games_categories.get(nome , [])
+   return render_template("category.html", categoria=nome , games=games_list)
+
+def all_categories():
+    return "" // TODO  #retornar todas as categorias
+
 
 ## Rota Blog
 @app.route("/blog")
